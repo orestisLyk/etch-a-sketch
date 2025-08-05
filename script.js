4f46f7be71b5b9370cf27e789bdc4fa493d84bec
orestisLyk <orestis.lyk@outlook.com>
2025-08-05 16:57:16 +0300

    Finish desired functionality

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
+const body = document.querySelector("body");
 const mainContainer = document.querySelector(".main-container");
+const startBtn = document.querySelector("#start-button");
+const slider = document.querySelector("#dimension-input");
+const dimensionDisplay = document.querySelector("#dimension-display");
 
-let DimensionInBlocks = 16;
+let dimensionInBlocks = 16;
+const maxDimension = 64;
+let isMouseDown = false;
+
+//on load
+slider.min = 0;
+slider.max = maxDimension;
+
+body.addEventListener("click",clickHandler);
+body.addEventListener("mousedown",() => {isMouseDown = true;})
+body.addEventListener("mouseup",() => {isMouseDown = false;})
+mainContainer.addEventListener("mouseover",etcher);
+
 
 //possibly redundant
 // function blockStyle(block,stringColor) {
 //     block.style.backgroundColor = stringColor;
 // }
+
+function clickHandler(click) {
+    const id = click.target.id;
+    if(id === "start-button") {
+        //switch following line to equivalent function that doesn't use innerhtml(for security)
+        mainContainer.innerHTML = "";
+        startBtn.textContent = "Refresh"
+        createBlockMatrix(dimensionInBlocks);
+    } else if (id === "dimension-input") {
+        dimensionInBlocks = slider.value;
+        dimensionDisplay.textContent = dimensionInBlocks; 
+    }
+}
+
+
+function etcher(event) {
+    const targetClasslist = event.target.classList;
+    if( targetClasslist.contains("block") && isMouseDown && !targetClasslist.contains("darkened-twice")) {
+        if(targetClasslist.contains("darkened")) {
+            targetClasslist.add("darkened-twice");
+        } else {
+            targetClasslist.add("darkened");
+        };
+    };
+}
 
 function createBlockRow(numberOfBlocks) {
     let div;
